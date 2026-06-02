712d33da8079b0da5bc333bd2aafa9606029b057
Arne Sostack <arne@whee.dk>
2026-06-02 21:47:08 +0200

    Migration to add ProcessedAtUtc

--- TreeCampaign.Repository/Migrations/20260602194602_AddProcessedAtToStoredDomainEvents.Designer.cs
+++ TreeCampaign.Repository/Migrations/20260602194602_AddProcessedAtToStoredDomainEvents.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TreeCampaign.Repository;
 
@@ -11,9 +12,11 @@ using TreeCampaign.Repository;
 namespace TreeCampaign.Repository.Migrations
 {
     [DbContext(typeof(TreeCampaignContext))]
-    partial class TreeCampaignContextModelSnapshot : ModelSnapshot
+    [Migration("20260602194602_AddProcessedAtToStoredDomainEvents")]
+    partial class AddProcessedAtToStoredDomainEvents
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.8");
