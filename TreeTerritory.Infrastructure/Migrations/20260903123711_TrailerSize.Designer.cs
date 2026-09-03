2034e02aaf933b50b58a195830747b282817c136
Arne Sostack <arne@whee.dk>
2026-09-03 15:37:28 +0200

    Fixed migrations after trailersize change

--- TreeTerritory.Infrastructure/Migrations/20260903123711_TrailerSize.Designer.cs
+++ TreeTerritory.Infrastructure/Migrations/20260903123711_TrailerSize.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TreeTerritory.Infrastructure;
 
@@ -11,9 +12,11 @@ using TreeTerritory.Infrastructure;
 namespace TreeTerritory.Infrastructure.Migrations
 {
     [DbContext(typeof(TreeTerritoryContext))]
-    partial class TreeTerritoryContextModelSnapshot : ModelSnapshot
+    [Migration("20260903123711_TrailerSize")]
+    partial class TrailerSize
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TreeTerritory.Infrastructure/Migrations/20260903123711_TrailerSize.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TreeTerritory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrailerSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "MaxTrailerSize",
+                table: "StreetSections",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)1,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldDefaultValue: (byte)2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "MaxTrailerSize",
+                table: "StreetSections",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)2,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldDefaultValue: (byte)1);
+        }
+    }
+}
